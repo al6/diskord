4bd6f0f29e7c8d3aebd8b4aa6e3d98c1b8dc558b
Albert Lee <aclee94@gmail.com>
2020-02-08 17:30:42 -0500

    Added application controller auth pattern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_member, :logged_in?
+  def current_member
+    return nil unless session[:session_token]
+    @current_member ||= Member.find_by(session_token: session[:session_token])
+  end
+
+  def ensure_logged_in
+    redirect_to new_session_url unless current_member
+  end
+
+  def login(member)
+    session[:session_token] = member.session_token
+  end
+  
+  def logout
+    current_member.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_member
+  end
 end
