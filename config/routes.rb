f522ebea995200c76319ad683d9e8de6613ef36b
Albert Lee <aclee94@gmail.com>
2020-02-08 18:01:46 -0500

    Default format json in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     resources :members, only: [:create]
     resource :session, only: [:create, :destroy]
   end
