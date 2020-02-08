fbd9d62bb09458d78f56edaf9f406f206d616e7f
Albert Lee <aclee94@gmail.com>
2020-02-08 17:05:58 -0500

    Member model auth pattern

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,39 @@
 class Member < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    member = Member.find_by(username: username)
+    return nil unless member
+    member.is_password?(password) ? member : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.generate_session_token
+  end
+
+  def generate_session_token
+    return SecureRandom.urlsafe_base64(16)
+  end    
 end
