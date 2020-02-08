5f1dbe06caa5a710c385abc68d31483ba86ff594
Albert Lee <aclee94@gmail.com>
2020-02-08 17:53:35 -0500

    Added create and destroy routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resource :session, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 end
