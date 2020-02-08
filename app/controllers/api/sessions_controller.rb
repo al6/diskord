e09399baf9daa34740568ea2d76f0ca65e7fef61
Albert Lee <aclee94@gmail.com>
2020-02-08 18:17:07 -0500

    Sessions controller auth pattern

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @member = Member.find_by_credentials(
+      params[:member][:username],
+      params[:member][:password]
+    )
+
+    if @member
+      login(@member)
+      render "api/members/show"
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    @member = current_member
+    if @member
+      logout
+      render {}
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
 end
