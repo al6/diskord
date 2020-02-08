bb51cd162babac255ce9a94af280c960cce1be1a
Albert Lee <aclee94@gmail.com>
2020-02-08 17:59:37 -0500

    Removed member files

--- app/controllers/members_controller.rb
+++
@@ -1,4 +0,0 @@
-class MembersController < ApplicationController
-  def create
-  end
-end
--- test/controllers/members_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MembersControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get members_create_url
-    assert_response :success
-  end
-
-end
