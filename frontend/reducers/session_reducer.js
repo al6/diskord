8e1b95867f4d43339f0edaa3ce7114b9782e1a80
Albert Lee <aclee94@gmail.com>
2020-02-08 22:04:55 -0500

    Made sure to match correct action key

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -8,7 +8,7 @@ const sessionReducer = (state = default_state, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_MEMBER:
-      return { id: action.currentUser.id };
+      return { id: action.member.id };
     case LOGOUT_CURRENT_USER:
       return default_state;
     default:
