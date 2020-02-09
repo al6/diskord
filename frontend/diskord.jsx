62a14a64f345be9bb6e8567c404dd816b8ef60da
Albert Lee <aclee94@gmail.com>
2020-02-08 22:08:30 -0500

    Made file names more readable

--- frontend/diskord.jsx
+++ frontend/diskord.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import { postMember, postSession, deleteSession } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  window.signup = postMember;
+  window.login = postSession;
+  window.logout = deleteSession;
   ReactDOM.render(<h1>Welcome to Diskord!</h1>, root);
 });
