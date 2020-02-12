f496a16a882369482863778d17e4a8bf209f904c
Albert Lee <aclee94@gmail.com>
2020-02-11 23:40:25 -0500

    Fixed vertical align and background image

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, HashRouter, Switch } from 'react-router-dom';
 import LoginFormContainer from './session_form/login_form/login_form_container';
 import RegisterFormContainer from './session_form/register_form/register_form_container';
 import SplashContainer from './splash/splash_container';
@@ -8,11 +8,11 @@ import SplashContainer from './splash/splash_container';
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <div>
+      <Switch>
         <Route exact path="/" component={SplashContainer}/>
         <Route exact path="/login" component={LoginFormContainer} />
         <Route exact path="/register" component={RegisterFormContainer} />
-      </div>
+      </Switch>
     </HashRouter>
   </Provider>
 );
--- frontend/components/session_form/login_form/login_form.jsx
+++ frontend/components/session_form/login_form/login_form.jsx
@@ -20,46 +20,48 @@ class LoginForm extends React.Component {
   render() {
     const {responseJSON} = this.props.errors
     return(
-      <div className="login-container">
-        <div className="form-top">
-          <div className="form-header">Welcome back!</div>
-          <div className="excited">We're so excited to see you again!</div>
-        </div>
-        <form className="login-form" onSubmit={this.handleSubmit}>
-          <div className="email-container">
-            <div className="input-label-and-errors">
-              <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>EMAIL</div>
-              <div className="login-error">{responseJSON}</div>
+      <div className="login-page">
+        <div className="login-container">
+          <div className="form-top">
+            <div className="form-header">Welcome back!</div>
+            <div className="excited">We're so excited to see you again!</div>
+          </div>
+          <form className="login-form" onSubmit={this.handleSubmit}>
+            <div className="email-container">
+              <div className="input-label-and-errors">
+                <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>EMAIL</div>
+                <div className="login-error">{responseJSON}</div>
+              </div>
+              <input 
+                className={ responseJSON ? "error-form-input" : "form-input" }
+                type="text"
+                onChange={this.update('email')}
+                value={this.state.email}
+              />
             </div>
-            <input 
-              className={ responseJSON ? "error-form-input" : "form-input" }
-              type="text"
-              onChange={this.update('email')}
-              value={this.state.email}
-            />
-          </div>
-          <div className="password-container">
-            <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>PASSWORD</div>
-            <input 
-              className={ responseJSON ? "error-form-input" : "form-input" }
-              type="password" 
-              onChange={this.update('password')}
-              value={this.state.password}
-            />
-          </div>
-          <div className="form-link-container">
-            <Link className="form-link" to={``}>Forgot your password?</Link>
-          </div>
-          <button className="login-form-button">
-            <div className="login-form-button-text">Login</div>
-          </button>
-          <div className="form-link-container">
-            <div className="register-link-helper">
-              Need an account?
-            </div> 
-            <Link className="form-link" to={``}>Register</Link>
-          </div>
-        </form>
+            <div className="password-container">
+              <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>PASSWORD</div>
+              <input 
+                className={ responseJSON ? "error-form-input" : "form-input" }
+                type="password" 
+                onChange={this.update('password')}
+                value={this.state.password}
+              />
+            </div>
+            <div className="form-link-container">
+              <Link className="form-link" to={``}>Forgot your password?</Link>
+            </div>
+            <button className="login-form-button">
+              <div className="login-form-button-text">Login</div>
+            </button>
+            <div className="form-link-container">
+              <div className="register-link-helper">
+                Need an account?
+              </div> 
+              <Link className="form-link" to={``}>Register</Link>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
