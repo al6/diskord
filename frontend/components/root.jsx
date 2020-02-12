2a7078f556a18258b89d544f6ff76590f5055569
Albert Lee <aclee94@gmail.com>
2020-02-12 09:56:54 -0500

    Removed unused imports

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route, HashRouter, Switch } from 'react-router-dom';
-import LoginFormContainer from './session_form/login_form/login_form_container';
-import RegisterFormContainer from './session_form/register_form/register_form_container';
-import SplashContainer from './splash/splash_container';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 const Root = ({ store }) => (
   <Provider store={store}>
