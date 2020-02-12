f18a9ad9e8ed7fd6cb608b6cbe5d8c39a206f0c4
Albert Lee <aclee94@gmail.com>
2020-02-12 09:56:28 -0500

    Refactored to simplify and separate concerns

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,15 +4,11 @@ import { Route, HashRouter, Switch } from 'react-router-dom';
 import LoginFormContainer from './session_form/login_form/login_form_container';
 import RegisterFormContainer from './session_form/register_form/register_form_container';
 import SplashContainer from './splash/splash_container';
-
+import App from './App';
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <Switch>
-        <Route exact path="/" component={SplashContainer}/>
-        <Route exact path="/login" component={LoginFormContainer} />
-        <Route exact path="/register" component={RegisterFormContainer} />
-      </Switch>
+      <App/>
     </HashRouter>
   </Provider>
 );
