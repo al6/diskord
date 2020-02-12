c9b1a004261e5261075274cfe8fa629991ce7f42
Albert Lee <aclee94@gmail.com>
2020-02-12 11:05:42 -0500

    Preparing for ProtectedRoute

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -10,6 +10,7 @@ const App = () => (
     <Switch>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/register" component={RegisterFormContainer} />
+      
       <Route exact path="/" component={SplashContainer}/>
     </Switch>
   </div>
