6468700721a4213812a4c1413cf21f57835bfc1b
Albert Lee <aclee94@gmail.com>
2020-02-12 09:56:06 -0500

    Removed unused imports

--- frontend/diskord.jsx
+++ frontend/diskord.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-import { register, login, logout } from './actions/session_actions';
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentMember) {
