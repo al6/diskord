47cbcbf229db73aafcbdf3eb3202eef877d1c756
Albert Lee <aclee94@gmail.com>
2020-02-12 09:54:59 -0500

    Auth route util file

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props =>
+      !loggedIn ? <Component {...props} /> : <Redirect to="/channels/@me" />
+    }
+  />
+);
+
+const mapStateToProps = state => {
+  return { loggedIn: Boolean(state.session.id) };
+};
+
+export const AuthRoute = withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(Auth)
+);
