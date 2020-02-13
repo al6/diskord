9e16e25943c48fc934b3c825e246f148825256bc
Albert Lee <aclee94@gmail.com>
2020-02-12 20:21:20 -0500

    Index create and destroy methods for guilds controller

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,9 +1,35 @@
 class GuildsController < ApplicationController
-  # def create
+  def index
+    @guilds = Guild.all
+    render :index
+  end
+  
+  def create
+    @guild = Guild.new(guild_params)
+    if @guild.save
+      render :show
+    else
+      render json: ["Sorry, that guild name is taken!"], status: 409
+    end
+  end
 
-  # end
+  def destroy
+    current_id = current_member.id
+    if current_id
+      guild = Guild.find_by(owner_id: guild_params[:owner_id])
+      if guild
+        guild.destroy
+        return
+      else
+        render json: ["Guild not found"], status: 404
+      end 
+    else
+      render json: ["That's not your guild!"], status: 401
+    end
+  end
 
-  # def destroy
-
-  # end
+  private
+  def guild_params
+    params.require(:guild).permit(:name, :owner_id)
+  end
 end
