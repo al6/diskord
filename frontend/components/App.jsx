342def107de356d454e9df4eafb4b2bbb9f4f681
Albert Lee <aclee94@gmail.com>
2020-02-13 09:40:24 -0500

    Reinstated file

---
+++ frontend/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AppButton from "./app_button/app_button";
+import RegisterFormContainer from '../components/session_form/register_form/register_form_container';
+import LoginFormContainer from '../components/session_form/login_form/login_form_container';
+import SplashContainer from '../components/splash/splash_container';
+import { Route, Switch } from 'react-router-dom';
+import { AuthRoute } from "../util/route_util";
+const App = () => (
+  <div className="app-container">
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/register" component={RegisterFormContainer} />
+      <Route exact path="/" component={SplashContainer}/>
+    </Switch>
+  </div>
+);
+
+export default App;
