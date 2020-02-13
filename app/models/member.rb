f478d4e3203966af5fb1ecea6a0b217c49f4b637
Albert Lee <aclee94@gmail.com>
2020-02-13 11:27:47 -0500

    Model level validation for email presence true

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
-  validates :username, :password_digest, :session_token, presence: true
+  validates :email, :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   
