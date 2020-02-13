4848dd51c0c51370a54c6ff681c642cab793c634
Albert Lee <aclee94@gmail.com>
2020-02-13 12:55:34 -0500

    Added routes for guilds, guild memberships, channels, and messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     resources :members, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :guilds, only: [:index, :create, :destroy]
+    resources :guild_memberships, only: [:index, :create, :destroy]
+    resources :channels, only: [:get, :post, :delete]
+    resources :messages, only: [:index, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
