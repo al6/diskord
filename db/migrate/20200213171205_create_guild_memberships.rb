30dea34ede4711225be9e40b658171c8ac933296
Albert Lee <aclee94@gmail.com>
2020-02-13 13:01:33 -0500

    Migrated guild memberships table

--- db/migrate/20200213171205_create_guild_memberships.rb
+++ db/migrate/20200213171205_create_guild_memberships.rb
@@ -3,7 +3,6 @@ class CreateGuildMemberships < ActiveRecord::Migration[5.2]
     create_table :guild_memberships do |t|
       t.integer :member_id
       t.integer :guild_id
-
       t.timestamps
     end
   end
