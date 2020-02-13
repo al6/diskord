1ab45e1ed470316de2c147d0835d24ff78e558a2
Albert Lee <aclee94@gmail.com>
2020-02-13 17:39:48 -0500

    Added protected route for channels route

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,12 +3,14 @@ import RegisterFormContainer from '../components/session_form/register_form/regi
 import LoginFormContainer from '../components/session_form/login_form/login_form_container';
 import SplashContainer from '../components/splash/splash_container';
 import { Route, Switch } from 'react-router-dom';
-import { AuthRoute } from "../util/route_util";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import GuildMembershipsIndexContainer from '../components/guild_memberships_index/guild_memberships_index_container';
 const App = () => (
   <div className="app-container">
     <Switch>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/register" component={RegisterFormContainer} />
+      <ProtectedRoute exact path="/channels/@me" component={GuildMembershipsIndexContainer}/>
       <Route exact path="/" component={SplashContainer}/>
     </Switch>
   </div>
