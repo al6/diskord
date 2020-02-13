9766c0cfb61bae34eaf19b4bc4b742d7a79650d1
Albert Lee <aclee94@gmail.com>
2020-02-13 09:33:09 -0500

    Added case for removing session errors

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,10 +1,12 @@
-import { RECEIVE_CURRENT_MEMBER, RECEIVE_SESSION_ERRORS } from "../actions/session_actions";
+import { RECEIVE_CURRENT_MEMBER, RECEIVE_SESSION_ERRORS, REMOVE_SESSION_ERRORS } from "../actions/session_actions";
 
 const sessionErrorsReducer = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
       return action.errors
     case RECEIVE_CURRENT_MEMBER:
+      return [];
+    case REMOVE_SESSION_ERRORS:
       return [];
     default:
       return state;
