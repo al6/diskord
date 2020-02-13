4d0e406b824a0e07768b572910b9f9b5f4e65959
Albert Lee <aclee94@gmail.com>
2020-02-12 20:27:38 -0500

    Added index, create, destroy routes for guilds

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :members, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :guilds, only: [:index, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
