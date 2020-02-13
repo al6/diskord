97b79b8886fff8f8436c435bb6a59da5ba00e1f7
Albert Lee <aclee94@gmail.com>
2020-02-13 14:02:41 -0500

    Removed unnecessary whitespace

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -14,11 +14,7 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
 
 const Protected = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
-     loggedIn ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to="/login" />
-    )
+    loggedIn ? <Component {...props} /> : <Redirect to="/login" />
   )} />
 );
 
