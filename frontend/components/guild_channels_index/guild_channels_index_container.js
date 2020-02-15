f2596fe84282ac860d8498e15a6cae7bb87cf590
Albert Lee <aclee94@gmail.com>
2020-02-15 08:38:14 -0500

    Mapped in currently selected guild based on route

--- frontend/components/guild_channels_index/guild_channels_index_container.js
+++ frontend/components/guild_channels_index/guild_channels_index_container.js
@@ -2,8 +2,9 @@ import { connect } from 'react-redux';
 import GuildChannelsIndex from './guild_channels_index';
 import { logout } from '../../actions/session_actions';
 
-const mapStateToProps = state => ({
-  currentMemberId: state.session.id
+const mapStateToProps = (state, ownProps) => ({
+  currentMemberId: state.session.id,
+  guildId: ownProps.match.params.guildId,
 });
 
 const mapDispatchToProps = dispatch => ({
