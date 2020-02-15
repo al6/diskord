0f6b9e42efae9574d7a4139bbcc166aaa3932659
Albert Lee <aclee94@gmail.com>
2020-02-15 07:32:18 -0500

    Added corresponding actions for member

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,8 +3,14 @@ class Member < ApplicationRecord
   validates :email, uniqueness: true, 'valid_email_2/email': true
   validates :password, length: { minimum: 6, allow_nil: true }
   
+  has_many :guild_memberships
+  has_many :guilds,
+    through: :guild_memberships,
+    source: :guild
+
   after_initialize :ensure_session_token
   attr_reader :password
+  
 
   def self.find_by_credentials(email, password)
     member = Member.find_by(email: email)
