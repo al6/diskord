7c57c36da5490c3318325e9c3b7858d7d49bf3aa
Albert Lee <aclee94@gmail.com>
2020-02-15 07:45:22 -0500

    Removed console logs

--- frontend/reducers/guilds_reducer.js
+++ frontend/reducers/guilds_reducer.js
@@ -1,22 +1,16 @@
 import { RECEIVE_GUILD, RECEIVE_GUILDS } from "../actions/guild_membership_actions";
 
 const guildsReducer = (state = [], action) => {
-  // debugger
   Object.freeze(state);
-  // console.log(state)
-  // console.log(action)
   let newState;
   switch (action.type) {
     case RECEIVE_GUILD:
-      console.log("receiveguild")
       newState = JSON.parse(JSON.stringify(state));
       return newState.concat(action.guild)
     case RECEIVE_GUILDS:
       newState = JSON.parse(JSON.stringify(state));
-      console.log(newState)
       return newState.concat(action.guilds)
     default:
-      console.log("default case")
       return state;
   }
 };
