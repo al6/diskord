c7a1a050ce41475804d20eab7539df5f3a81640a
Albert Lee <aclee94@gmail.com>
2020-02-15 13:28:20 -0500

    Created compound modal component for customization and re-use later on

---
+++ frontend/components/modal/modal.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import ModalPortal from "./modal_portal";
+
+const ModalContext = React.createContext();
+
+const Content = ({ children, ...otherProps }) => (
+  <ModalContext.Consumer>
+    {({ isOpen, closeModal }) => {
+      if (!isOpen) {
+        return <div></div>;
+      }
+
+      return (
+        <ModalPortal>
+          <div onClick={closeModal} className="modal-content-container">
+            <div
+              onClick={e => e.stopPropagation()}
+              className="modal-content"
+              {...otherProps}
+            >
+              {children}
+            </div>
+          </div>
+        </ModalPortal>
+      );
+    }}
+  </ModalContext.Consumer>
+);
+
+const CloseButton = ({ children, ...otherProps }) => (
+  <ModalContext.Consumer>
+    {({ closeModal }) => {
+      return (
+        <div onClick={closeModal} {...otherProps}>
+          {children}
+        </div>
+      );
+    }}
+  </ModalContext.Consumer>
+);
+
+const OpenButton = ({ children, ...otherProps }) => (
+  <ModalContext.Consumer>
+    {({ openModal }) => {
+      return (
+        <div onClick={openModal} {...otherProps}>
+          {children}
+        </div>
+      );
+    }}
+  </ModalContext.Consumer>
+);
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal(newVal) {
+    this.setState({ modalIsOpen: newVal });
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <ModalContext.Provider
+        value={{
+          isOpen: this.state.modalIsOpen,
+          openModal: () => this.toggleModal(true),
+          closeModal: () => this.toggleModal(false)
+        }}
+      >
+        {children}
+      </ModalContext.Provider>
+    );
+  }
+}
+
+Modal.OpenButton = OpenButton;
+Modal.CloseButton = CloseButton;
+Modal.Content = Content;
+
+export default Modal;
