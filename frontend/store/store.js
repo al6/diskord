3dd9a0fdb7e595cbc3cd89bf7c4f1821f5caa34d
Albert Lee <aclee94@gmail.com>
2020-02-14 19:29:02 -0500

    Redux devtools setup

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default configureStore;
