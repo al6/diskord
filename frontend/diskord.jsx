5f45e13a3b1b320af06f3a847558bc6639f2d9b1
Albert Lee <aclee94@gmail.com>
2020-02-15 07:29:49 -0500

    Removed testing functions from window

--- frontend/diskord.jsx
+++ frontend/diskord.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-import { create } from './util/guild_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
@@ -18,7 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-  window.createGuild = create;
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
