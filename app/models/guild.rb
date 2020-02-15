c87879db6bb340dbdedf5f65ec199d23f959fc20
Albert Lee <aclee94@gmail.com>
2020-02-15 07:31:51 -0500

    Added associations for guild

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -2,6 +2,9 @@ class Guild < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, :owner_id, presence: true
 
+  has_many :guild_memberships
+  has_many :guild_members,
+    through: :guild_memberships
   belongs_to :member,
     foreign_key: :owner_id
 end
