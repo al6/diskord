16a17986ab3a342d19d4fbe844e2526d55a1a919
Albert Lee <aclee94@gmail.com>
2020-02-15 09:05:36 -0500

    Remove logger from prod

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,7 +5,9 @@ import rootReducer from "../reducers/root_reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const middleware = document.location.host.indexOf('localhost') >= 0 ? [thunk, logger] : [thunk]
+
 const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk, logger)));
+  createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(...middleware)));
 
 export default configureStore;
