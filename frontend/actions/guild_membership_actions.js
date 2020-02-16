8143afd37a988be36d21d310d163040164d1ed57
Albert Lee <aclee94@gmail.com>
2020-02-15 20:31:11 -0500

    Prettier formatting

--- frontend/actions/guild_membership_actions.js
+++ frontend/actions/guild_membership_actions.js
@@ -1,35 +1,38 @@
-import * as GuildMembershipAPIUtil from '../util/guild_membership_api_util';
-import * as GuildAPIUtil from '../util/guild_api_util';
+import * as GuildMembershipAPIUtil from "../util/guild_membership_api_util";
+import * as GuildAPIUtil from "../util/guild_api_util";
 
-export const RECEIVE_GUILD_MEMBERSHIP = 'RECEIVE_GUILD_MEMBERSHIP';
-export const RECEIVE_GUILD = 'RECEIVE_GUILD';
-export const RECEIVE_GUILDS = 'RECEIVE_GUILDS';
-  
+export const RECEIVE_GUILD_MEMBERSHIP = "RECEIVE_GUILD_MEMBERSHIP";
+export const RECEIVE_GUILD = "RECEIVE_GUILD";
+export const RECEIVE_GUILDS = "RECEIVE_GUILDS";
+
 const receiveGuild = guild => ({
   type: RECEIVE_GUILD,
   guild
-})
+});
 
 const receiveGuilds = guilds => ({
   type: RECEIVE_GUILDS,
   guilds
-})
+});
 
 const receiveGuildMembership = guild_membership => ({
   type: RECEIVE_GUILD_MEMBERSHIP,
   guild_membership
-})
+});
 
-export const createGuildMembership = guild_membership => dispatch => GuildMembershipAPIUtil.create(guild_membership)
-  .then(guild_membership => dispatch(receiveGuildMembership(guild_membership)));
-  
+export const createGuildMembership = guild_membership => dispatch =>
+  GuildMembershipAPIUtil.create(guild_membership).then(guild_membership =>
+    dispatch(receiveGuildMembership(guild_membership))
+  );
+
 export const createGuild = guild => dispatch => {
-  debugger
-  return GuildAPIUtil.create(guild)
-    .then(guild => dispatch(receiveGuild(guild)))
+  return GuildAPIUtil.create(guild).then(guild =>
+    dispatch(receiveGuild(guild))
+  );
 };
 
 export const fetchGuildMemberships = id => dispatch => {
-  return GuildMembershipAPIUtil.fetchMemberships(id)
-    .then(guilds => dispatch(receiveGuilds(guilds)))
+  return GuildMembershipAPIUtil.fetchMemberships(id).then(guilds =>
+    dispatch(receiveGuilds(guilds))
+  );
 };
