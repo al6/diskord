0344dea8b83f99efc1b05f1ef13d85ba5835078d
Albert Lee <aclee94@gmail.com>
2020-02-16 15:03:53 -0500

    Fixed bug where channels were being duplicated in redux state

--- frontend/reducers/channels_reducer.js
+++ frontend/reducers/channels_reducer.js
@@ -2,14 +2,13 @@ import { RECEIVE_CHANNEL, RECEIVE_CHANNELS } from "../actions/channel_actions";
 
 const channelsReducer = (state = [], action) => {
   Object.freeze(state);
-  let newState;
+  let newState, channelIds;
   switch (action.type) {
     case RECEIVE_CHANNEL:
       newState = JSON.parse(JSON.stringify(state));
       return newState.concat(action.channel);
     case RECEIVE_CHANNELS:
-      newState = JSON.parse(JSON.stringify(state));
-      return newState.concat(action.channels);
+      return action.channels;
     default:
       return state;
   }
