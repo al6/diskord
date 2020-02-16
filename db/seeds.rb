aa11ca625f62a8ae2793bef5658ce68de94c508d
Albert Lee <aclee94@gmail.com>
2020-02-16 13:18:32 -0500

    Fixed hardcoded ids for db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,6 @@ Guild.delete_all
 GuildMembership.delete_all
 tommy = Member.create!(email: "tommy@fakemail.com", username: "tommy", password: "hunter2")
 trader_joes_fanatic = Member.create!(email: "tjfan1@fakemail.com", username: "tjfan1", password: "hunter3")
-aA = Guild.create!(name: "App Academy", owner_id: 1)
-aA_instructors = Guild.create!(name: "App Academy Instructors", owner_id: 1)
-aA_membership = GuildMembership.create!(member_id: 1, guild_id: 1)
-aA_instructors_membership = GuildMembership.create!(member_id: 1, guild_id: 2)
-
+aA = Guild.create!(name: "App Academy", owner_id: tommy.id)
+aA_instructors = Guild.create!(name: "App Academy Instructors", owner_id: tommy.id)
 
