3578a128c8df46c4d8fb119656293990e1efffec
Albert Lee <aclee94@gmail.com>
2020-02-15 22:20:01 -0500

    Email input type email

--- frontend/components/session_form/register_form/register_form.jsx
+++ frontend/components/session_form/register_form/register_form.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class RegisterForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {email: '', username: '', password: ''}
+    this.state = { email: "", username: "", password: "" };
+    this.state.errors = { email: "", username: "", password: "" };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -18,55 +19,84 @@ class RegisterForm extends React.Component {
   }
 
   render() {
-    const {removeErrors} = this.props;
+    const { removeErrors } = this.props;
+    // console.log(this.props.errors);
     let responseJSON = Object.values(this.props.errors);
     let errors;
-    responseJSON.length > 0 ? errors = responseJSON.join(", ") : responseJSON = undefined;
-    return(
-        <div className="session-page">
+    responseJSON.length > 0
+      ? (errors = responseJSON.join(", "))
+      : (responseJSON = undefined);
+    return (
+      <div className="session-page">
         <div className="register-container">
-        <h2 className="form-header">Create an account</h2>
-        <form className="register-form" onSubmit={this.handleSubmit}>
-          <div className="email-container">
-          <div className="input-label-and-errors">
-                <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>EMAIL</div>
+          <h2 className="form-header">Create an account</h2>
+          <form className="register-form" onSubmit={this.handleSubmit}>
+            <div className="email-container">
+              <div className="input-label-and-errors">
+                <div
+                  className={
+                    responseJSON
+                      ? "errors-form-input-label"
+                      : "form-input-label"
+                  }
+                >
+                  EMAIL
+                </div>
                 <div className="login-error">{errors}</div>
               </div>
-            <input 
-              className={ responseJSON ? "error-form-input" : "form-input" }
-              type="text"
-              onChange={this.update('email')}
-              value={this.state.email}
-            />
-          </div>
-          <div className="username-input-container">
-            <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>USERNAME</div>              
-            <input 
-              className={ responseJSON ? "error-form-input" : "form-input" }
-              type="text"
-              onChange={this.update('username')}
-              value={this.state.username}
-            />
-          </div>
-          <div className="password-container">
-            <div className={ responseJSON ? "errors-form-input-label" : "form-input-label"}>PASSWORD</div>
-            <input 
-              className={ responseJSON ? "error-form-input" : "form-input" }
-              type="password" 
-              onChange={this.update('password')}
-              value={this.state.password}
-            />
-          </div>
-          <button className="register-form-button">
-            <div className="register-form-button-text">Register</div>
-          </button>
-          <div className="form-link-container">
-            <Link className="form-link" to={`/login`} onClick={()=>removeErrors()}>Already have an account?</Link>
-          </div>
-        </form>
+              <input
+                className={responseJSON ? "error-form-input" : "form-input"}
+                type="email"
+                onChange={this.update("email")}
+                value={this.state.email}
+              />
+            </div>
+            <div className="username-input-container">
+              <div
+                className={
+                  responseJSON ? "errors-form-input-label" : "form-input-label"
+                }
+              >
+                USERNAME
+              </div>
+              <input
+                className={responseJSON ? "error-form-input" : "form-input"}
+                type="text"
+                onChange={this.update("username")}
+                value={this.state.username}
+              />
+            </div>
+            <div className="password-container">
+              <div
+                className={
+                  responseJSON ? "errors-form-input-label" : "form-input-label"
+                }
+              >
+                PASSWORD
+              </div>
+              <input
+                className={responseJSON ? "error-form-input" : "form-input"}
+                type="password"
+                onChange={this.update("password")}
+                value={this.state.password}
+              />
+            </div>
+            <button className="register-form-button">
+              <div className="register-form-button-text">Register</div>
+            </button>
+            <div className="form-link-container">
+              <Link
+                className="form-link"
+                to={`/login`}
+                onClick={() => removeErrors()}
+              >
+                Already have an account?
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
