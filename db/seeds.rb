129531f81eb683bdbbb6692d0212b58a6b5c3529
Albert Lee <aclee94@gmail.com>
2020-02-16 15:04:11 -0500

    Removed unnecessary seed logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@ tommy = Member.create!(email: "tommy@fakemail.com", username: "tommy", password:
 trader_joes_fanatic = Member.create!(email: "tjfan1@fakemail.com", username: "tjfan1", password: "hunter3")
 aA = Guild.create!(name: "App Academy", owner_id: tommy.id)
 aA_instructors = Guild.create!(name: "App Academy Instructors", owner_id: tommy.id)
-
