19cd570c80e94c2d5cbc23ed008804fac1303b54
Albert Lee <aclee94@gmail.com>
2020-02-16 15:04:56 -0500

    Added after_create lifecycle methods. When a guild is created, that guild will also have a guild membership and default channel created for it

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -2,10 +2,21 @@ class Guild < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, :owner_id, presence: true
 
+  after_create :create_default_channel, :create_default_guild_membership
+
   has_many :channels
   has_many :guild_memberships
   has_many :guild_members,
     through: :guild_memberships
-  belongs_to :member,
+  belongs_to :owner,
+    class_name: :Member,
     foreign_key: :owner_id
+  
+  def create_default_channel
+    Channel.create(name: 'general', guild_id: self.id)
+  end
+
+  def create_default_guild_membership
+    self.guild_memberships.create(member_id: self.owner.id)
+  end
 end
