160caa91445e7ad550742bdeb12ac8c1882cbe27
Albert Lee <aclee94@gmail.com>
2020-02-16 19:40:20 -0500

    Validations for messages model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   validates :body, :author_id, :channel_id, presence: true
-  belongs_to :
+  validates :body, length: {maximum: 100}
+  
+  belongs_to :author,
+    class_name: :Member,
+    foreign_key: :author_id,
+    primary_key: :id
+
+  belongs_to :channel
 end
