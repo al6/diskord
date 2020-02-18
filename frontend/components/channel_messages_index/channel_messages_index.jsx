ad83e47810b38dcc0651997237e36a0f7b8e92af
Albert Lee <aclee94@gmail.com>
2020-02-17 22:15:57 -0500

    Fixed really strange same date recurrence bug

--- frontend/components/channel_messages_index/channel_messages_index.jsx
+++ frontend/components/channel_messages_index/channel_messages_index.jsx
@@ -44,39 +44,47 @@ class ChannelMessagesIndex extends React.Component {
 
   render() {
     const { messages = [], channel = [] } = this.props;
+    debugger;
     return (
       <div className="channel-messages-index">
         <div className="channel-messages-index-header">
           <div className="channel-messages-index-header-left-right">
             <div className="channel-messages-index-header-left">
-              <span className="channel-messages-index-header-left-hashtag">#</span>
-              <span className="channel-messages-index-header-left-channel-name">{channel.name}</span>
+              <span className="channel-messages-index-header-left-hashtag">
+                #
+              </span>
+              <span className="channel-messages-index-header-left-channel-name">
+                {channel.name}
+              </span>
             </div>
           </div>
           <span></span>
         </div>
         <div className="channel-messages-index-main">
-          {messages.map(message => (
-            <div key={message.id}>
-              <div className="message-width-container">
-                <div className="message-container">
-                  <div className="message-profile-picture"></div>
-                  <div className="message-data">
-                    <div className="message-header">
-                      <span className="message-author">{message.author}</span>
-                      <span className="recent-date">
-                        {Date(message.created_at)
-                          .split(" ")
-                          .slice(0, 5)
-                          .join(" ")}
-                      </span>
+          {messages.map(message => {
+            return (
+              <div key={message.id}>
+                <div className="message-width-container">
+                  <div className="message-container">
+                    <div className="message-profile-picture"></div>
+                    <div className="message-data">
+                      <div className="message-header">
+                        <span className="message-author">{message.author}</span>
+                        <span className="recent-date">
+                          {new Date(Date.parse(message.created_at))
+                            .toString()
+                            .split(" ")
+                            .slice(0, 5)
+                            .join(" ")}
+                        </span>
+                      </div>
+                      <div className="message-body">{message.body}</div>
                     </div>
-                    <div className="message-body">{message.body}</div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
           <div ref={this.messagesIndex}></div>
         </div>
         <div className="channel-messages-index-footer">
