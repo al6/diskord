4066947be9baa460167022f079f126be097d3bb2
Albert Lee <aclee94@gmail.com>
2020-02-18 17:59:11 -0500

    Prettier formatting

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,4 @@
-import * as APIUtil from '../util/session_api_util';
+import * as APIUtil from "../util/session_api_util";
 export const RECEIVE_CURRENT_MEMBER = "RECEIVE_CURRENT_MEMBER";
 export const LOGOUT_CURRENT_MEMBER = "LOGOUT_CURRENT_MEMBER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
@@ -9,32 +9,35 @@ const receiveCurrentMember = member => ({
 });
 
 const logoutCurrentMember = () => ({
-  type: LOGOUT_CURRENT_MEMBER,
+  type: LOGOUT_CURRENT_MEMBER
 });
 
-const receiveErrors = errors => ({
+export const receiveErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
-  errors,
+  errors
 });
 
 export const removeErrors = () => ({
   type: REMOVE_SESSION_ERRORS
-})
+});
 
 export const register = member => dispatch => {
-  return APIUtil.register(member)
-    .then(member => dispatch(receiveCurrentMember(member)),
-    errors => dispatch(receiveErrors(errors.responseJSON)))
+  return APIUtil.register(member).then(
+    member => dispatch(receiveCurrentMember(member)),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  );
 };
 
 export const login = member => dispatch => {
-  return APIUtil.login(member)
-    .then(member => dispatch(receiveCurrentMember(member)),
-    errors => dispatch(receiveErrors(errors.responseJSON)))
+  return APIUtil.login(member).then(
+    member => dispatch(receiveCurrentMember(member)),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  );
 };
 
 export const logout = () => dispatch => {
-  return APIUtil.logout().then(() => location.reload())
+  return APIUtil.logout().then(() => location.reload());
 };
 
-export const demologin = () => dispatch => dispatch(login({ email: 'tommy@fakemail.com', password: 'hunter2' }))
+export const demologin = () => dispatch =>
+  dispatch(login({ email: "tommy@fakemail.com", password: "hunter2" }));
